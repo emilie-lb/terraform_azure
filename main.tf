2d3eaa31c6f3f31c3959d0b1f615a2c1c6134b8a
emilie-lb <emilielb75@gmail.com>
2021-10-08 11:09:50 +0200

    test infrasturture as code!

---
+++ main.tf
@@ -0,0 +1,24 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "=2.46.0"
+    }
+  }
+    backend "azurerm" {
+        resource_group_name  = "tform-lemilie"
+        storage_account_name = "briefoctober"
+        container_name       = "contenerbriefoctober"
+        key                  = "terraform.tfstate"
+    }
+
+}
+
+# provider "azurerm" {
+#   features {}
+# }
+
+# resource "azurerm_resource_group" "state-demo-secure" {
+#   name     = "state-demo"
+#   location = "francecentral"
+# }
